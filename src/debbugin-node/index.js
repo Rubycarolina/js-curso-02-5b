0a0f3aa5f669b13274df80c0d1d3f4c7506c72a1
Rubycarolina <rubycarolinadelacruzbasora11-gmail.com>
2021-03-26 20:48:23 -0400

    pruebas error

--- src/debbugin-node/index.js
+++ src/debbugin-node/index.js
@@ -4,7 +4,7 @@
 const ordenaBurbuja = (lista) => {
 
 
-    let longitudlista, indexOrder, indexChande, auxChange;
+    let longitudlista, indexOrder, indexhange, auxChange;
     longitudlista =lista.length;
 
     // console.info(longitudlista); // Mostramos por consola la cantidad de elementos en una lista
@@ -13,9 +13,9 @@ const ordenaBurbuja = (lista) => {
         //For animado.
         for ( indexChange = 0; indexChange < ( longitudlista - indexOrder); indexChange++) {
             // Intercambiamos los valores si el anterior es mayor al siguiente.
-            if (lista[indexChande] > lista[indexChange + 1]) {
+            if (lista[indexChange] > lista[indexChange + 1]) {
                auxChange = lista[indexChange];
-                lista[indexChange] = lista[indexChange + 1];
+                lista[indexChange] = lista[indexChange + 1]; 
                 lista[indexChange + 1] = auxChange; 
             }
                 
@@ -23,15 +23,14 @@ const ordenaBurbuja = (lista) => {
         }
     }
 
-    returnlista;
+    return lista;
 
-
+}
    let listaDesordenada = [6, 5, 3, 1, 8,7, 2,4];
-    ole.warn(listaDesordenada); //Mostramos, por consola, la lista desordenada
+    console.warn(listaDesordenada); //Mostramos, por consola, la lista desordenada
     
     const listaOrdenada = ordenaBurbuja( listaDesordenada);
-    ole.log(listaOrdenada); //Mostrar, por consola, la lista ya ordenada
+    console.log(listaOrdenada); //Mostrar, por consola, la lista ya ordenada
 
                
 
-}
